9c2c064cab21c7c8de6de0e6694bdff7247a8fee
cyb3rdoc <mastermind1024@gmail.com>
2022-11-30 14:59:50 +0400

    Update user_commands.bash

--- eos/user_commands.bash
+++ eos/user_commands.bash
@@ -7,5 +7,5 @@ nvidia-inst --series 390 -t
 # If no errors, install nvidia 390 drivers (Change parameters to 470 for 470 cards)
 nvidia-inst --series 390
 #pacman -Syuq --noconfirm --noprogressbar --needed nvidia-390xx-dkms nvidia-390xx-utils nvidia-390xx-settings nvidia-installer-kernel-para
-# Reconfigure grub
+# Regenerate bootloader config
 grub-mkconfig -o /boot/grub/grub.cfg
